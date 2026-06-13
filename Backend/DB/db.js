0647c16889e733dc5638e9499ed3c1cde3b4b1f2
SR101234 <sharadbansal67@gmail.com>
2026-06-13 20:10:30 +0530

    vercel json

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "root",        // your MySQL username
-  password: "",        // empty because no password
-  database: "office",    // replace with your database name
+  host: process.env.DB_HOST || "localhost", // replace with your MySQL host
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",        // your MySQL username
+  password: process.env.DB_PASSWORD || "",        // empty because no password
+  database: process.env.DB_NAME || "office",    // replace with your database name
   waitForConnections: true,
   connectionLimit: 10
 });
