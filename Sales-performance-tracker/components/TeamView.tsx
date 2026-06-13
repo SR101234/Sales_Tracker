8c9b89e608730bc51dfab0225f039e8e8be92fa0
SR101234 <sharadbansal67@gmail.com>
2026-06-13 21:26:59 +0530

    agent APi

--- Sales-performance-tracker/components/TeamView.tsx
+++ Sales-performance-tracker/components/TeamView.tsx
@@ -10,6 +10,7 @@ const TeamView = () => {
 
   const fetchAgents = async () => {
     try {
+      console.log("API URL:", import.meta.env.VITE_API_URL);
       const res = await fetch(`${import.meta.env.VITE_API_URL}/agent`);
       const data = await res.json();
 
